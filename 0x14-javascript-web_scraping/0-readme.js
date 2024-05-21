4eb1d19ff5265bde89c48d702138526fe35b5e1a
muhammad1094 <muhammadussamaa94@gmail.com>
2024-05-21 11:55:00 -0700

    d

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,11 @@
-#!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
